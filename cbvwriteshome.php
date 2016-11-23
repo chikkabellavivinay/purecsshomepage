9d6415a524a58f07ae2cd769cf123c01b491e2ee
Vinay S <vinay.s.9993@gmail.com>
2016-11-23 12:47:29 +0530

    Chikkabellavi Vinay Writes..

---
+++ cbvwriteshome.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="css/style.css" />
+<style>
+
+</style>
+</head>
+<body id="vinayaka">
+<div id="navbar">
+<div id="logo">
+LOGO
+</div><!--logoends-->
+<div id="navone">
+NAV1
+
+</div>
+<div id="navtwo">
+NAV2
+
+</div>
+<div id="navthree">
+
+NAV3
+</div>
+login/reg
+</div><!--navbarends-->
+
+
+
+
+
+<!--laptop-->
+<div id="abox">
+<div id="aboxone">
+<div id="boxfour"><center>APPLICATION PROGRAMMING</center></div>
+<div id="boxfive"><hr>[][][][][][][][][]<hr style="margin-top:50px;">[___________]
+[___________]<hr><hr>
+</div>
+<hr><hr><hr><hr>
+<div id="boxsix"><center><hr>MOBILE APPS DEV<hr></center></div>
+</div>
+<hr style="padding:25px; width:260px; margin-left:-10px; border-radius: 5px 5px 5px 5px ; background:gray; box-shadow:0px 0px 10px 0px skyblue;"><hr>
+<h1><center>Chikkabellavi Vinay Writes..</center></h1><div id="boxone">WEB DEV
+<hr id="hrpencil">
+<div id="boxthree"></div>
+<hr id="hrpencil"> 
+<div id="boxthree"></div>
+<hr id="hrpencil"> 
+<div id="boxthree"></div>
+
+</div>
+
+<div id="boxtwo">
+
+</div>
+
+
+</div>
+<div id="parenthr">
+
+  
+
+</div>
+<!--laptop design ends-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+</script>
+</body>
+</html>
